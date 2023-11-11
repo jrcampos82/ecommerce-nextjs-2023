e5bf991dfa7543095ecf04226a85548a3fd78488
Beto <jrcampos82@gmail.com>
2023-11-11 08:48:05 -0400

    Add provider directory to Tailwind content

---
+++ frontend/src/profile/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react';
+import { Tabs } from 'antd';
+// import CategoriesList from './components/CategoriesList';
+
+function Profile() {
+  return (
+    <div>
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Products" key="1">
+          Products
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Categories" key="2">
+          {/* <CategoriesList /> */}
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Orders" key="3">
+          Orders
+        </Tabs.TabPane>
+
+        <Tabs.TabPane tab="Users" key="4">
+          Users
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
+}
+
+export default Profile;
