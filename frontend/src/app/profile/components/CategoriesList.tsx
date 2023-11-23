4b1f6e6da4668ad666243c8fc7526620b21a02b7
Beto <jrcampos82@gmail.com>
2023-11-23 10:15:22 -0400

    Revert "Add table styling and category list with API"
    This reverts commit 410e655565d25f049bcbd6e8f1fcbc998e833e24.

--- frontend/src/app/profile/components/CategoriesList.tsx
+++ frontend/src/app/profile/components/CategoriesList.tsx
@@ -1,53 +1,11 @@
-import { Button, Table, message } from 'antd';
-import { useEffect, useState } from 'react';
+import { Button } from 'antd';
+import { useState } from 'react';
 import CategoryForm from './CategoryForm';
-import axios from 'axios';
-import moment from 'moment';
 
 function CategoriesList() {
   const [showCategoryForm, setShowCategoryForm] = useState(false);
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const getCategories = async () => {
-    try {
-      setLoading(true);
-      const res = await axios.get('http://localhost:3000/category');
-      setCategories(res.data);
-    } catch (error: any) {
-      message.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getCategories();
-  }, []);
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-    },
-    {
-      title: 'Created At',
-      dataIndex: 'createdAt',
-      key: 'createdAt',
-      render: (createdAt: string) => moment(createdAt).format('DD MMM YYYY'),
-    },
-    {
-      title: 'Action',
-      dataIndex: 'action',
-      key: 'action',
-    },
-  ];
 
   return (
     <div>
@@ -60,24 +18,15 @@ function CategoriesList() {
         >
           Add Category
         </Button>
-      </div>
 
-      <div className="mt-5">
-        <Table
-          dataSource={categories}
-          columns={columns}
-          loading={loading}
-          pagination={false}
-        />
+        {showCategoryForm && (
+          <CategoryForm
+            showCategoryForm={showCategoryForm}
+            setShowCategoryForm={setShowCategoryForm}
+            reloadData={() => {}}
+          ></CategoryForm>
+        )}
       </div>
-
-      {showCategoryForm && (
-        <CategoryForm
-          showCategoryForm={showCategoryForm}
-          setShowCategoryForm={setShowCategoryForm}
-          reloadData={() => {}}
-        ></CategoryForm>
-      )}
     </div>
   );
 }
